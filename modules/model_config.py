bfc7f958ba2c64364e71298a102509fa3217e79c
VishSeran <vishwa.sovis@gmail.com>
2026-07-01 14:29:32 +0530

    get response from model fn updated

---
+++ modules/model_config.py
@@ -0,0 +1,42 @@
+from transformers import AutoProcessor, AutoModelForImageTextToText
+import torch
+
+from modules.logger import get_logger
+
+logger = get_logger("model-config")
+VISION_MODEL = 'HuggingFaceTB/SmolVLM2-2.2B-Instruct'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+vision_model = AutoModelForImageTextToText.from_pretrained(
+    VISION_MODEL,
+    torch_dtype = torch.bfloat16
+).to(device)
+
+vision_processor = AutoProcessor.from_pretrained(VISION_MODEL)
+
+
+def get_response_from_model(encoded_images, user_query):
+    
+    try:
+        
+        if not encoded_images:
+            raise ValueError("Image inputs must not be empty")
+        
+        if not user_query:
+            raise ValueError("User query must not be empty")
+        
+        system_message = "You are a helpful assistant. Answer the following user query in 1 or 2 sentences: "
+
+    except ValueError as e:
+        logger.error(f"Value error: {e}")
+        raise
+    
+    except Exception as e:
+        logger.error(f"user query fetch error: : {e}")
+        raise 
+    
+
+
+
+
+
